ac17044da51b7cfbd1ba950becd6ce615363eef0
Chaitanya Sharma <chaitanya.sharma2@incedoinc.com>
2023-02-22 23:28:07 +0530

    b

--- src/App.js
+++ src/App.js
@@ -33,7 +33,9 @@ const App = () => {
                     <div className={
                         `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu?'md:ml-72':'flex-2'}`
                     }>
-                        <div className='fixed md'></div>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            NavBar
+                        </div>
                     </div>
                 </div>
             </BrowserRouter>
